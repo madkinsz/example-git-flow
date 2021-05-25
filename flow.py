b85d9ef4fa7eacba8ddee5ef1eacd48f08a9612d
Michael Adkins <michael@prefect.io>
2021-05-25 12:23:59 -0500

    Register via CLI

--- flow.py
+++ flow.py
@@ -3,6 +3,4 @@ from prefect.storage import GitHub
 
 hello_flow.storage = GitHub(repo="madkinsz/example-git-flow", path="flow.py")
 
-if __name__ == "__main__":
-   hello_flow.register("default")
-
+# To register flow, run: `prefect register -p flow.py --project default`
