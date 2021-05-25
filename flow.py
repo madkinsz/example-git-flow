b7f9d2a16af4d0e225b5c8f8ce0e695ebcfd54b6
Michael Adkins <michael@prefect.io>
2021-05-25 12:28:45 -0500

    Add non-imported flow example

--- flow.py
+++ flow.py
@@ -1,6 +1,13 @@
-from prefect.hello_world import hello_flow
 from prefect.storage import GitHub
+from prefect import task, Flow
 
-hello_flow.storage = GitHub(repo="madkinsz/example-git-flow", path="flow.py")
+@task(log_stdout=True)
+def say_hello():
+   print("Hello world!")
+
+with Flow("hello-world") as flow:
+   say_hello()
+
+flow.storage = GitHub(repo="madkinsz/example-git-flow", path="flow.py", ref="self-defined-flow")
 
 # To register flow, run: `prefect register -p flow.py --project default`
